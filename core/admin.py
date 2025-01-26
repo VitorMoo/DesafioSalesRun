383b8a20013655aade376527115ac5f273691f39
Vitor Moreira <viitornmoreira@outlook.com>
2025-01-26 16:02:24 -0300

    refactor: optimize admin display for Challenge and UserChallenge models

--- core/admin.py
+++ core/admin.py
@@ -3,10 +3,11 @@ from .models import Challenge, UserChallenge
 
 @admin.register(Challenge)
 class ChallengeAdmin(admin.ModelAdmin):
-    list_display = ['name', 'created_at']
+    list_display = ('name', 'points')
+    search_fields = ('name',)
+
 
 @admin.register(UserChallenge)
 class UserChallengeAdmin(admin.ModelAdmin):
-    list_display = ['user', 'challenge', 'status', 'points']
-    list_filter = ['status', 'challenge']
-    search_fields = ['user__username', 'user__cpf', 'challenge__name']
+    list_display = ('user', 'challenge', 'status')
+    search_fields = ('user__username', 'challenge__name')

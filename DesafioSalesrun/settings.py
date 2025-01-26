d0eaedb4e327596113513bd4ad885a7555667e3f
Vitor Moreira <viitornmoreira@outlook.com>
2025-01-26 16:04:33 -0300

    refactor: Update redirect URLs and configure static files handling

--- DesafioSalesrun/settings.py
+++ DesafioSalesrun/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = 'accounts.Users'
 
-LOGIN_REDIRECT_URL = '/core/my-challenges/'
+LOGIN_REDIRECT_URL = '/my-challenges/'
 
 LOGOUT_REDIRECT_URL = '/'
 
@@ -128,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

6657b9d6f95ca534186fc27a8e11109e22e6ff7c
Vitor Moreira <viitornmoreira@outlook.com>
2025-01-26 16:02:51 -0300

    feat: Add total_points field to User model

--- accounts/models.py
+++ accounts/models.py
@@ -8,6 +8,8 @@ class Users(AbstractUser):
     )
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='corretor')
     cpf = models.CharField(max_length=14, unique=True, blank=True, null=True)
+    total_points = models.PositiveIntegerField(default=0)
+
 
     def __str__(self):
         return f'{self.username} ({self.get_role_display()})'

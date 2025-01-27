936a8316c5dca3758864a4ca7bd2de61035ba02e
Vitor Moreira <viitornmoreira@outlook.com>
2025-01-27 00:22:01 -0300

    feat: Add separate settings and URLs for admin and portal
    Created distinct settings files (`settings_admin.py` and `settings_portal.py`) to configure environments for the admin and portal, including session cookie names and root URL configurations. Added `urls_admin.py` for dedicated admin URL routing.

---
+++ DesafioSalesrun/settings_admin.py
@@ -0,0 +1,8 @@
+from .settings import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+SESSION_COOKIE_NAME = 'admin_sessionid'
+
+ROOT_URLCONF = 'DesafioSalesrun.urls_admin'
+
---
+++ DesafioSalesrun/settings_portal.py
@@ -0,0 +1,5 @@
+from .settings import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+SESSION_COOKIE_NAME = 'portal_sessionid'

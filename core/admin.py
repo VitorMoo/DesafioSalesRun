161195b9625800b183dc02f2a0748d00ab90bf9b
Vitor Moreira <viitornmoreira@outlook.com>
2025-01-26 22:14:01 -0300

    feat: Add custom form for UserChallenge in admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,13 +1,25 @@
+from django import forms
 from django.contrib import admin
 from .models import Challenge, UserChallenge
+from accounts.models import Users
+
+class UserChallengeForm(forms.ModelForm):
+    class Meta:
+        model = UserChallenge
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['user'].queryset = Users.objects.all()
+        self.fields['user'].label_from_instance = lambda obj: f"{obj.cpf or 'Sem CPF'}"
 
 @admin.register(Challenge)
 class ChallengeAdmin(admin.ModelAdmin):
     list_display = ('name', 'points')
     search_fields = ('name',)
 
-
 @admin.register(UserChallenge)
 class UserChallengeAdmin(admin.ModelAdmin):
+    form = UserChallengeForm
     list_display = ('user', 'challenge', 'status')
-    search_fields = ('user__username', 'challenge__name')
+    search_fields = ('user__cpf', 'challenge__name')

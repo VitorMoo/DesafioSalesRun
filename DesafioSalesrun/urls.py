c98df968844795001ebea8f305139e3cdfac71b8
Vitor Moreira <viitornmoreira@outlook.com>
2025-01-26 22:18:20 -0300

    feat: Add Grappelli, media files handling, and update URLs

--- DesafioSalesrun/urls.py
+++ DesafioSalesrun/urls.py
@@ -1,8 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('core/', include('core.urls')),
+    path('', include('core.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

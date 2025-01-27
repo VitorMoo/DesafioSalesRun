340539c963b33aef0cbad43a06800a3055a90168
Vitor Moreira <viitornmoreira@outlook.com>
2025-01-27 10:04:31 -0300

    refactor: Remove admin and portal-specific settings and URLs.

--- DesafioSalesrun/settings_admin.py
+++
@@ -1,8 +0,0 @@
-from .settings import *
-
-DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-SESSION_COOKIE_NAME = 'admin_sessionid'
-
-ROOT_URLCONF = 'DesafioSalesrun.urls_admin'
-
--- DesafioSalesrun/settings_portal.py
+++
@@ -1,5 +0,0 @@
-from .settings import *
-
-DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-SESSION_COOKIE_NAME = 'portal_sessionid'
